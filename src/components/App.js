638653d76c4c463191905f145123118de13c012b
kehinde bukola <kehinde.bukola14@gmail.com>
2023-04-16 00:50:57 +0100

    css fixcommit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
@@ -20,15 +20,17 @@ function App() {
       </header> */}
 
 <div className="container">
-    <img className="logo" src="./images/logo.svg" alt="Logo" />
+    <img className="logo" src="./src/images/logo.svg" alt="Logo" />
 
     <div className="flex-container">
       <div className="left">
-        <img src="./images/illustration-mockups.svg" alt="" />
+        <img src="images/illustration-mockups.svg" alt="" />
       </div>
       <div className="right">
         <h1>Build The Community Your Fans Will Love</h1>
-        <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion</p>
+        <p>Huddle re-imagines the way we build communities. 
+          You have a voice, but so does your audience. 
+          Create connections with your users as you engage in genuine discussion</p>
         <button className="btn">Register</button>
       </div>
     </div>
@@ -47,7 +49,9 @@ function App() {
   </div>
 
   <footer>
-    <p className="attribution">Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.Coded by <a href="#">Daniel Ducas-Viramontes</a>.</p>
+    <p className="attribution">Challenge by 
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.Coded by 
+    <a href="#">Daniel Ducas-Viramontes</a>.</p>
   </footer>
     </div>
   );
